3dd29377e2796ed99ca97f852fa07634854b4197
secretMoi <win.moi@hotmail.com>
2021-03-07 21:21:47 +0100

    Ajoutez des fichiers projet.

---
+++ Pictura.ClientAndroid/Pictura.ClientAndroid/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Pictura.ClientAndroid.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Pictura.ClientAndroid.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
---
+++ Pictura.ClientAndroid/Pictura.ClientAndroid/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Pictura.ClientAndroid.Models;
+using Pictura.ClientAndroid.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pictura.ClientAndroid.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+		public Item Item { get; set; }
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+			BindingContext = new NewItemViewModel();
+		}
+	}
+}
