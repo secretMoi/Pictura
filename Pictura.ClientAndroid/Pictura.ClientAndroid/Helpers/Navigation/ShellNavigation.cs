4ac849de5040d83d39cd8583433c6bd8a4293f76
Quentin <stagiaire63@rtl.be>
2021-03-27 14:10:32 +0100

    Ajout license syncfusion Ajout event swipedown pour une image sélectionné edans la gallerie

---
+++ Pictura.ClientAndroid/Pictura.ClientAndroid/Helpers/Navigation/ShellNavigation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Pictura.ClientAndroid.Helpers.Routes;
+using Xamarin.Forms;
+
+namespace Pictura.ClientAndroid.Helpers.Navigation
+{
+	public class Navigation : INavigation
+	{
+		private readonly IRoute _route;
+		
+		public Navigation(IRoute route)
+		{
+			_route = route ?? throw new ArgumentNullException(nameof(route));
+		}
+		
+		public Task AsRootPage(Page page)
+		{
+			if (page == null) throw new ArgumentNullException(nameof(page));
+			
+			return Shell.Current.GoToAsync("//" + page);
+		}
+
+		public void ChangeFlow(Page page)
+		{
+			Application.Current.MainPage = page ?? throw new ArgumentNullException(nameof(page));
+		}
+		
+		public void GoToMainFlow()
+		{
+			ChangeFlow(new AppShell());
+		}
+		
+		public void GoToLogInFlow()
+		{
+			//ChangeFlow(new LogInPage());
+		}
+
+		public Task PushAsync<T>() where T : Page
+		{
+			return Shell.Current.GoToAsync(_route.RouteName(typeof(T).ToString()));
+		}
+		
+		/**
+		 * <summary>Navigue vers une page avec paramètres</summary>
+		 * <param name="parameterName">Nom du paramètre</param>
+		 * <param name="data">Données à transmettre à la nouvelle page</param>
+		 */
+		public Task PushAsync<T>(string parameterName, object data) where T : Page
+		{
+			if (parameterName == null) throw new ArgumentNullException(nameof(parameterName));
+			if (data == null) throw new ArgumentNullException(nameof(data));
+
+			return Shell.Current.GoToAsync(
+				_route.RouteName(typeof(T).ToString()) +
+				"?" +
+				parameterName +
+				"=" +
+				data
+			);
+		}
+	}
+}
---
+++ Pictura.ClientAndroid/Pictura.ClientAndroid/Helpers/Routes/IRoute.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace Pictura.ClientAndroid.Helpers.Routes
+{
+	public interface IRoute
+	{
+		void RegisterRoute<T>() where T : Page;
+		string RouteName(string completeNamespace);
+	}
+}
