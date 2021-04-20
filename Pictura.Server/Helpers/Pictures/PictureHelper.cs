35afaf073132bf20ee4df9f6e0a8c61e9bf50efa
Quentin <stagiaire63@rtl.be>
2021-04-20 21:15:42 +0200

    Ajout upload d'images sur le serveur

--- Pictura.Server/Helpers/Pictures/PictureHelper.cs
+++ Pictura.Server/Helpers/Pictures/PictureHelper.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Pictura.Server.Helpers.Json;
+using Pictura.Server.Models;
 
 namespace Pictura.Server.Helpers.Pictures
 {
---
+++ Pictura.Server/Services/File/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Pictura.Server.Services.File
+{
+	public interface IFileService
+	{
+		Task SaveFileAsync(IFormFile file);
+		Task SaveFilesAsync(IEnumerable<IFormFile> files);
+	}
+}
