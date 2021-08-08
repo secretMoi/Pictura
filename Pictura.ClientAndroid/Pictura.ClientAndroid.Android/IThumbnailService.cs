66a9b73729ce5c64da3090de31fc36fa222d4bdd
Quentin <stagiaire63@rtl.be>
2021-08-08 15:59:55 +0200

    Génération thumbnails pour image OK DI pour la partie Android OK

--- Pictura.ClientAndroid/Pictura.ClientAndroid.Android/IThumbnailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Pictura.ClientAndroid.Droid
-{
-	public interface IThumbnailService
-	{
-		Task<string> GenerateThumbnailAsync(string rootPath, string filePath);
-	}
-}
---
+++ Pictura.ClientAndroid/Pictura.ClientAndroid/PlatformInterfaces/IThumbnailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Pictura.ClientAndroid.PlatformInterfaces
+{
+	public interface IThumbnailService
+	{
+		Task<string> GenerateThumbnailAsync(string thumbnailsFolderPath, string filePath);
+	}
+}
